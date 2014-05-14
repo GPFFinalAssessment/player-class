e6f61d298da433dfb1b6c1576ff74204bb87d338
snowy844 <snowy844@gmail.com>
2014-05-14 18:37:18 +0800

    player class

--- PlayerClass.cs
+++ PlayerClass.cs
@@ -3,43 +3,35 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Storage;
 using Microsoft.Xna.Framework.Input;
-namespace player
+
+namespace GPF_Final_Assessment
 {
 	public class Player
 	{
 		public Texture2D texture;
 		public Vector2 position;
-
 		public Game1 game;
+		public float movementSpeed;
 
-
-		public Keys W;
-		public Keys S;
-		public Keys A;
-		public Keys D;
-
-
-		public Player ( Texture2D texture, Vector2 pos)
+		public Player(Texture2D texture, Vector2 pos)
 		{
-
 			this.texture = texture;
 			this.position = pos;
-
+			this.movementSpeed = 2f;
 		}
 
-		public void Update (GameTime gametime)
+		public void Update(GameTime gametime, GraphicsDeviceManager gdManager)
 		{
-			if (Keyboard.GetState ().IsKeyDown (Keys.W))  position.Y -= 0.5f;
-			if (Keyboard.GetState ().IsKeyDown (Keys.S))  position.Y += 0.5f;
-			if (Keyboard.GetState ().IsKeyDown (Keys.A))  position.X -= 0.5f;
-			if (Keyboard.GetState ().IsKeyDown (Keys.D))  position.X += 0.5f;
-
-
+			if (Keyboard.GetState().IsKeyDown(Keys.Up) && position.Y > 120) position.Y -= movementSpeed;
+			if (Keyboard.GetState().IsKeyDown(Keys.Down) && (position.Y < (gdManager.PreferredBackBufferHeight - movementSpeed - this.texture.Height))) position.Y += movementSpeed;
+			//if (Keyboard.GetState().IsKeyDown(Keys.A)) position.X -= 0.5f;
+			//if (Keyboard.GetState().IsKeyDown(Keys.D)) position.X += 0.5f;
 		}
-		public void Draw (SpriteBatch spritebatch)
+
+		public void Draw(SpriteBatch spritebatch)
 		{
-			Vector2 offset = new Vector2 (texture.Width / 2, 0);
-			spritebatch.Draw (texture, position - offset, Color.White);
+			Vector2 offset = new Vector2(texture.Width / 2, 0);
+			spritebatch.Draw(texture, position - offset, Color.White);
 		}
 	}
 }
